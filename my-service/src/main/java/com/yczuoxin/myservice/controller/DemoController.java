be2ccfd2ad4c92db820b89687abf418a867c704c
ycxzuo <343720929@qq.com>
2023-02-24 00:04:45 +0800

    hutool的DB测试

--- my-service/src/main/java/com/yczuoxin/myservice/controller/DemoController.java
+++ my-service/src/main/java/com/yczuoxin/myservice/controller/DemoController.java
@@ -1,5 +1,7 @@
 package com.yczuoxin.myservice.controller;
 
+import cn.hutool.db.DbUtil;
+import cn.hutool.db.Entity;
 import com.yczuoxin.myservice.dto.ResponseResult;
 import com.yczuoxin.myservice.service.DemoService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.sql.SQLException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/demo")
@@ -18,6 +22,12 @@ public class DemoController {
 
     @GetMapping("/default/{id}")
     public ResponseResult<String> getDefaultMessage(@PathVariable Long id) {
+        return ResponseResult.success(demoService.getDefaultData(id));
+    }
+
+    @GetMapping("/hutool/{id}")
+    public ResponseResult<String> getHutoolMessage(@PathVariable Long id) throws SQLException {
+        List<Entity> entities = DbUtil.use().query("select * from INFO where id = " + id);
         return ResponseResult.success(demoService.getDefaultData(id));
     }
 
