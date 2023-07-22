df4362adc16fa2cb5e154a10c0b9ffab619519f1
ycxzuo <343720929@qq.com>
2023-07-22 17:04:01 +0800

    策略枚举测试类提交

--- my-service/src/test/java/com/yczuoxin/myservice/enums/PayrollPayTest.java
+++ my-service/src/test/java/com/yczuoxin/myservice/enums/PayrollPayTest.java
@@ -1,5 +1,6 @@
 package com.yczuoxin.myservice.enums;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class PayrollPayTest {
@@ -8,6 +9,7 @@ class PayrollPayTest {
     void testPay() {
         System.out.println(PayrollDay.FRIDAY.pay(11 * 60, 2));
         System.out.println(PayrollDay.SUNDAY.pay(11 * 60, 2));
+        Assertions.assertNotEquals(PayrollDay.FRIDAY.pay(11 * 60, 2), PayrollDay.SUNDAY.pay(11 * 60, 2));
     }
 
 }
