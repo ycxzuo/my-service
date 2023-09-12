78e29bf15d6397664d84638351137cda733f24f9
ycxzuo <343720929@qq.com>
2023-09-12 10:38:34 +0800

    CompletableFuture 功能测试

--- my-service/src/test/java/com/yczuoxin/myservice/concurrent/executor/future/CompletableFutureTest.java
+++ my-service/src/test/java/com/yczuoxin/myservice/concurrent/executor/future/CompletableFutureTest.java
@@ -8,6 +8,11 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
+/**
+ * CompletableFuture 与 Stream 并行流的区别：
+ *  completableFuture 可以自定义线程池
+ *  Stream 使用的默认的 ForkJoinPool，核心线程数跟CPU核心数有关，不能自定义
+ */
 class CompletableFutureTest {
 
     SecureRandom secureRandom = new SecureRandom();
